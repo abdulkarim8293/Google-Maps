d6490e8925aeb10c13ecf8b22ac0635f14fe9393
unknown <abdulkarim8293@gmail.com>
2020-06-23 03:39:54 +0600

    add location sdk

--- app/src/main/java/com/abdulkarim/googlemaps/MainActivity.java
+++ app/src/main/java/com/abdulkarim/googlemaps/MainActivity.java
@@ -1,27 +1,81 @@
 package com.abdulkarim.googlemaps;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+
+    private static final int LOCATION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        initializeMap();
+        permissions();
+
+    }
+
+    private void permissions() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    // permission success
+                    getDeviceLocation();
+                } else {
+                    ActivityCompat.requestPermissions(this, permissions, LOCATION_REQUEST_CODE);
+                }
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_REQUEST_CODE);
+            }
+        }
+    }
+
+    private void getDeviceLocation() {
+        
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+                //getLastLocation();
+
+            } else {
+                //Permission not granted
+            }
+        }
+
+    }
+
+    private void initializeMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        LatLng latLng = new LatLng(23.7548804, 90.3742164);
+        googleMap.addMarker(new MarkerOptions().position(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
     }
 }
